02a5181020e29b884e50b1636f6beb4032cff960
wastadtlander <wastadt@google.com>
2022-07-06 20:40:48 +0000

    full implementation of integration test for model compiler

--- src/test/cc/wfa/virtual_people/training/model_compiler/compiler_main_test.cc
+++ src/test/cc/wfa/virtual_people/training/model_compiler/compiler_main_test.cc
@@ -12,18 +12,72 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <cstdlib>
+#include <fcntl.h>
+#include <string>
+
+#include "glog/logging.h"
 #include "gtest/gtest.h"
+#include "google/protobuf/text_format.h"
+#include "google/protobuf/io/zero_copy_stream_impl.h"
+#include "google/protobuf/util/message_differencer.h"
+#include "wfa/virtual_people/training/model_config.pb.h"
+
+using namespace ::google::protobuf;
+using namespace ::google::protobuf::io;
+using namespace ::google::protobuf::util;
 
 namespace wfa_virtual_people {
 namespace {
 
-TEST(ModelCompilerMainTest, PopulationNode) {}
+void ReadTextProtoFile(std::string path, Message& message) {
+    CHECK(!path.empty()) << "No path set";
+    int fd = open(path.c_str(), O_RDONLY);
+    CHECK(fd > 0) << "Unable to open file: " << path;
+    FileInputStream fstream(fd);
+    CHECK(TextFormat::Parse(&fstream, &message)) 
+        << "Unable to parse textproto file: " << path;
+}
 
-TEST(ModelCompilerMainTest, PopulationNodeRedistributeProbabilitiesForEmptyPools) {}
+bool ProtoEquals(std::string expectedPath, std::string outputPath) {
+    CompiledNode expected;
+    ReadTextProtoFile(expectedPath, expected);
 
-TEST(ModelCompilerMainTest, PopulationNodeKappaLessThanOne) {}
+    CompiledNode output;
+    ReadTextProtoFile(outputPath, output);
 
-TEST(ModelCompilerMainTest, PopulationNodeDiscretization) {}
+    MessageDifferencer diff;
+    return diff.Equals(expected, output);
+}
+
+
+TEST(ModelCompilerMainTest, PopulationNode) {
+    std::string expectedPath = "src/test/cc/wfa/virtual_people/training/model_compiler/test_data/compiled_node_for_population_node.textproto";
+    std::string outputPath = "src/test/cc/wfa/virtual_people/training/model_compiler/compiled_node_for_population_node.textproto";
+
+    EXPECT_TRUE(ProtoEquals(expectedPath, outputPath));
+}
+
+TEST(ModelCompilerMainTest, PopulationNodeRedistributeProbabilitiesForEmptyPools) {
+    std::string expectedPath = "src/test/cc/wfa/virtual_people/training/model_compiler/test_data/compiled_node_for_population_node_redistribute_probabilities_for_empty_pools.textproto";
+    std::string outputPath = "src/test/cc/wfa/virtual_people/training/model_compiler/compiled_node_for_population_node_redistribute_probabilities_for_empty_pools.textproto";
+
+    EXPECT_TRUE(ProtoEquals(expectedPath, outputPath));
+}
+
+TEST(ModelCompilerMainTest, PopulationNodeKappaLessThanOne) {
+    std::string expectedPath = "src/test/cc/wfa/virtual_people/training/model_compiler/test_data/compiled_node_for_population_node_kappa_less_than_one.textproto";
+    std::string outputPath = "src/test/cc/wfa/virtual_people/training/model_compiler/compiled_node_for_population_node_kappa_less_than_one.textproto";
+
+    EXPECT_TRUE(ProtoEquals(expectedPath, outputPath));
+}
+
+TEST(ModelCompilerMainTest, PopulationNodeDiscretization) {
+    std::string expectedPath = "src/test/cc/wfa/virtual_people/training/model_compiler/test_data/compiled_node_for_population_node_discretization.textproto";
+    std::string outputPath = "src/test/cc/wfa/virtual_people/training/model_compiler/compiled_node_for_population_node_discretization.textproto";
+
+    EXPECT_TRUE(ProtoEquals(expectedPath, outputPath));
+}
 
 }   // namespace
 }   // namespace wfa_virtual_people
