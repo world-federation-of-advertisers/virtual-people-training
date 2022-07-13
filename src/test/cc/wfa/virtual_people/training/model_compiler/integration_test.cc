fbcaf0cd2fd84bc0f8aa625ad3823d979dca805a
wastadtlander <wastadt@google.com>
2022-07-13 21:23:43 +0000

    integration testing framework + config for integration test for model compiler

---
+++ src/test/cc/wfa/virtual_people/training/model_compiler/integration_test.cc
@@ -0,0 +1,92 @@
+// Copyright 2022 The Cross-Media Measurement Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <string>
+#include <tuple>
+#include <vector>
+
+#include "absl/status/status.h"
+#include "common_cpp/protobuf_util/textproto_io.h"
+#include "glog/logging.h"
+#include "google/protobuf/text_format.h"
+#include "google/protobuf/util/message_differencer.h"
+#include "gtest/gtest.h"
+#include "tools/cpp/runfiles/runfiles.h"
+#include "wfa/virtual_people/training/config.pb.h"
+#include "wfa/virtual_people/training/model_config.pb.h" // temp for testing
+
+using bazel::tools::cpp::runfiles::Runfiles;
+using ::wfa::ReadTextProtoFile;
+
+namespace wfa_virtual_people {
+namespace {
+
+std::vector<std::tuple<std::string, std::string, std::string>> ParseConfig(std::string path) {
+    IntegrationTestList config;
+
+    absl::Status readConfigStatus = ReadTextProtoFile(path, config); // can do further error checking at some point with absl::Status
+    
+    std::vector<std::tuple<std::string, std::string, std::string>> targets;
+    std::string name, output, golden, error, rPath, execute;
+    std::unique_ptr<Runfiles> runfiles(Runfiles::CreateForTest(&error));
+
+    for (int testIndex = 0; testIndex < config.tests_size(); testIndex++) {
+        IntegrationTest it = config.tests().at(testIndex);
+        rPath = runfiles->Rlocation(it.binary());
+        for (int testCaseIndex = 0; testCaseIndex < it.test_cases_size(); testCaseIndex++) {
+            TestCase tc = it.test_cases().at(testCaseIndex);
+            name = it.name() + "_" + tc.name(); 
+            execute = rPath;
+            for (int binaryParameterIndex = 0; binaryParameterIndex < tc.binary_parameters_size(); binaryParameterIndex++) {
+                BinaryParameter bp = tc.binary_parameters().at(binaryParameterIndex);
+                execute += " --" + bp.name() + "=" + bp.value(); 
+                if (!bp.golden().empty()) { // check if file exists to determine mode
+                    output = bp.value();
+                    golden = bp.golden();
+                    targets.push_back({name, output, golden});
+                }
+            }
+            std::system(execute.c_str()); 
+        }
+    }
+
+    return targets;
+}
+
+class IntegrationTestParamaterizedFixture : public ::testing::TestWithParam<std::tuple<std::string, std::string, std::string>> {
+    protected:
+        google::protobuf::util::MessageDifferencer diff;    
+};
+
+TEST_P(IntegrationTestParamaterizedFixture, Test) { 
+    std::tuple<std::string, std::string, std::string> targetPair(GetParam());
+
+    CompiledNode output;
+    absl::Status outputStatus = ReadTextProtoFile(std::get<1>(targetPair), output);
+
+    CompiledNode golden;
+    absl::Status goldenStatus = ReadTextProtoFile(std::get<2>(targetPair), golden);
+
+    ASSERT_TRUE(diff.Equals(output, golden));
+}
+
+INSTANTIATE_TEST_SUITE_P(IntegrationTest, IntegrationTestParamaterizedFixture,
+    ::testing::ValuesIn(ParseConfig("src/test/cc/wfa/virtual_people/training/model_compiler/test_data/config.textproto")),
+    [](const ::testing::TestParamInfo<IntegrationTestParamaterizedFixture::ParamType>& info) {
+    std::string name = std::get<0>(info.param);
+    return name;   
+    });
+
+}  // namespace
+}  // namespace wfa_virtual_people
