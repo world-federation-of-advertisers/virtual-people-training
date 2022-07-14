1e404225a95c768cd08823b9da8017db637dc609
wastadtlander <wastadt@google.com>
2022-07-14 17:44:08 +0000

    golden mode fix and formatting to framework

--- src/test/cc/wfa/virtual_people/training/model_compiler/integration_test.cc
+++ src/test/cc/wfa/virtual_people/training/model_compiler/integration_test.cc
@@ -26,7 +26,7 @@
 #include "gtest/gtest.h"
 #include "tools/cpp/runfiles/runfiles.h"
 #include "wfa/virtual_people/training/config.pb.h"
-#include "wfa/virtual_people/training/model_config.pb.h" // temp for testing
+#include "wfa/virtual_people/training/model_config.pb.h"  // temp for testing
 
 using bazel::tools::cpp::runfiles::Runfiles;
 using ::wfa::ReadTextProtoFile;
@@ -34,64 +34,80 @@ using ::wfa::ReadTextProtoFile;
 namespace wfa_virtual_people {
 namespace {
 
-std::vector<std::tuple<std::string, std::string, std::string>> ParseConfig(std::string path) {
-    IntegrationTestList config;
+std::vector<std::tuple<std::string, std::string, std::string>> ParseConfig(
+    std::string path) {
+  IntegrationTestList config;
 
-    absl::Status readConfigStatus = ReadTextProtoFile(path, config);
-    
-    std::vector<std::tuple<std::string, std::string, std::string>> targets;
-    std::unique_ptr<Runfiles> runfiles(Runfiles::CreateForTest());
-    std::string name, output, golden, rPath, execute;
+  absl::Status readConfigStatus = ReadTextProtoFile(path, config);
 
-    for (int testIndex = 0; testIndex < config.tests_size(); testIndex++) {
-        IntegrationTest it = config.tests().at(testIndex);
-        rPath = runfiles->Rlocation(it.binary());
-        for (int testCaseIndex = 0; testCaseIndex < it.test_cases_size(); testCaseIndex++) {
-            TestCase tc = it.test_cases().at(testCaseIndex);
-            name = it.name() + "_" + tc.name(); 
-            execute = rPath;
-            for (int binaryParameterIndex = 0; binaryParameterIndex < tc.binary_parameters_size(); binaryParameterIndex++) {
-                BinaryParameter bp = tc.binary_parameters().at(binaryParameterIndex);
-                if (!bp.golden().empty() && open(bp.golden().data(), O_RDONLY) != -1) { 
-                    output = bp.value();
-                    golden = bp.golden();
-                    targets.push_back({name, output, golden});
-                } else execute += " --" + bp.name() + "=" + bp.golden();
-                /*
-                 * For this line ^ need to make it so when executed via system it actually generates the file.
-                 * The issue is that because the test is running via Bazel, Bazel doesn't let you write to your workspace.
-                 */
-                if (execute == rPath) execute += " --" + bp.name() + "=" + bp.value();
-            }
-            std::system(execute.c_str()); 
+  std::vector<std::tuple<std::string, std::string, std::string>> targets;
+  std::unique_ptr<Runfiles> runfiles(Runfiles::CreateForTest());
+  std::string name, output, golden, rPath, execute;
+
+  for (int testIndex = 0; testIndex < config.tests_size(); testIndex++) {
+    IntegrationTest it = config.tests().at(testIndex);
+    rPath = runfiles->Rlocation(it.binary());
+    for (int testCaseIndex = 0; testCaseIndex < it.test_cases_size();
+         testCaseIndex++) {
+      TestCase tc = it.test_cases().at(testCaseIndex);
+      name = it.name() + "_" + tc.name();
+      execute = rPath;
+      for (int binaryParameterIndex = 0;
+           binaryParameterIndex < tc.binary_parameters_size();
+           binaryParameterIndex++) {
+        BinaryParameter bp = tc.binary_parameters().at(binaryParameterIndex);
+        execute += " --" + bp.name() + "=" + bp.value();
+        if (!bp.golden().empty() && open(bp.golden().data(), O_RDONLY) != -1) {
+          output = bp.value();
+          golden = bp.golden();
+          targets.push_back({name, output, golden});
+        } else if (!bp.golden().empty()) {
+          execute.erase(execute.find(bp.value()));
+          execute += bp.golden();
         }
+        /*
+         * For this section ^ need to make it so when executed via system it
+         * actually generates the file. The issue is that because the test is
+         * running via Bazel, Bazel doesn't let you write to your workspace.
+         */
+      }
+      std::system(execute.c_str());
     }
+  }
 
-    return targets;
+  return targets;
 }
 
-class IntegrationTestParamaterizedFixture : public ::testing::TestWithParam<std::tuple<std::string, std::string, std::string>> {
-    protected:
-        google::protobuf::util::MessageDifferencer diff;    
+class IntegrationTestParamaterizedFixture
+    : public ::testing::TestWithParam<
+          std::tuple<std::string, std::string, std::string>> {
+ protected:
+  google::protobuf::util::MessageDifferencer diff;
 };
 
-TEST_P(IntegrationTestParamaterizedFixture, Test) { 
-    std::tuple<std::string, std::string, std::string> targetPair(GetParam());
+TEST_P(IntegrationTestParamaterizedFixture, Test) {
+  std::tuple<std::string, std::string, std::string> targetPair(GetParam());
 
-    CompiledNode output;
-    absl::Status outputStatus = ReadTextProtoFile(std::get<1>(targetPair), output);
+  CompiledNode output;
+  absl::Status outputStatus =
+      ReadTextProtoFile(std::get<1>(targetPair), output);
 
-    CompiledNode golden;
-    absl::Status goldenStatus = ReadTextProtoFile(std::get<2>(targetPair), golden);
+  CompiledNode golden;
+  absl::Status goldenStatus =
+      ReadTextProtoFile(std::get<2>(targetPair), golden);
 
-    ASSERT_TRUE(diff.Equals(output, golden));
+  ASSERT_TRUE(diff.Equals(output, golden));
 }
 
-INSTANTIATE_TEST_SUITE_P(IntegrationTest, IntegrationTestParamaterizedFixture,
-    ::testing::ValuesIn(ParseConfig("src/test/cc/wfa/virtual_people/training/model_compiler/test_data/config.textproto")),
-    [](const ::testing::TestParamInfo<IntegrationTestParamaterizedFixture::ParamType>& info) {
-    std::string name = std::get<0>(info.param);
-    return name;   
+INSTANTIATE_TEST_SUITE_P(
+    IntegrationTest, IntegrationTestParamaterizedFixture,
+    ::testing::ValuesIn(
+        ParseConfig("src/test/cc/wfa/virtual_people/training/model_compiler/"
+                    "test_data/config.textproto")),
+    [](const ::testing::TestParamInfo<
+        IntegrationTestParamaterizedFixture::ParamType>& info) {
+      std::string name = std::get<0>(info.param);
+      return name;
     });
 
 }  // namespace
